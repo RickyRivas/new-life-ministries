84ff66fc2a3ff3a9cf300c0450e727f04282b009
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-08-05 14:49:28 -0500

    .

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -25,23 +25,25 @@ document.querySelector('video').playbackRate = 1;
 // // Set a variable for our button element.
 // const scrollToTopButton = document.getElementById("js-top");
 
-// // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
-// const scrollFunc = () => {
-//   // Get the current scroll value
-//   let y = window.scrollY;
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
 
-//   // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-//   if (y > 0) {
-//     scrollToTopButton.className = "top-link show";
-//   } else {
-//     scrollToTopButton.className = "top-link hide";
-//   }
-// };
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
 
-// window.addEventListener("scroll", scrollFunc);
-// // Scroll Nav 
+window.addEventListener("scroll", scrollFunc);
+// Scroll Nav 
 
-// window.onscroll = () => {
-//   const nav = document.querySelector('#navbar');
-//   if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  const logop = document.querySelector('#logo-p');
+  if (this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+  if(this.scrollY <= 10) logop.className = ''; else logop.className = 'scroll-p';
 };
