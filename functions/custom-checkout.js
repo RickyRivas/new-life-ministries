11881351be05faa930f3f2e323e5bf1a8c78fdfa
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-09-30 22:10:20 -0500

    .

---
+++ functions/custom-checkout.js
@@ -0,0 +1,53 @@
+// init stripe with secret API key
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2020-03-02',
+    maxNetworkRetries: 2,
+});
+
+
+//
+
+exports.handler = async (e) => {
+    // grab the imported product
+    const importedProduct = e.body
+
+    // create session
+    const session = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_address_collection: {
+            allowed_countries: ['US', 'CA'],
+        },
+
+        //
+        success_url: `${process.env.URL}/success.html`,
+        cancel_url: `${process.env.URL}/cancel.html`,
+        line_items: [{
+            price_data: {
+                currency: 'usd',
+                unit_amount: product.amount,
+                product_data: {
+                    name: product.name,
+
+                    images: [product.image],
+                },
+            },
+            quantity: validatedQuantity,
+        }, ],
+        metadata: {
+            items: JSON.stringify([{
+                sku: product.sku,
+                name: product.name,
+
+            }, ]),
+        },
+    })
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            sessionId: session.id,
+            publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        }),
+    };
+}
--- public/scripts/customInput.js
+++ public/scripts/customInput.js
@@ -1,75 +1,50 @@
+const form = document.querySelector('.custom-payment-form');
+const input = document.querySelector('.custom-input')
+const output = document.querySelector('.payment-output')
 
-// custom input
-const customInputForm = document.querySelector('.custom-input-form')
 
-async function handleSubmit(e) {
-    e.preventDefault();
-    document
-        .querySelectorAll('button')
-        .forEach((button) => (button.disabled = true));
-
+form.addEventListener('submit', async (e) => {
     
-    // get the number inputs value and format the value to cents 
-    const enteredAmount = document.querySelector('.custom-amount').value
-    const formattedAmount = Math.round(enteredAmount * 100);
+    // Prev Def
+    e.preventDefault();
 
-    const product = {
-        sku: "custom amount",
-        name: 'custom amount',
-        amount: formattedAmount,
-        currency: "USD"
+    // grab the user custom amount
+    output.textContent = input.value;
+
+    // format the value to pennies so Stripe can accept the value
+    const formattedValue = input.value * 100
+
+    // create object upon click event 
+    const newProduct = {
+        sku: "Custom Amount",
+        name: "Custom Amount",
+        amount: formattedValue,
+        currency: "USD",
+        description: `Give a gift of ${ input.value } to New Life Ministries`,
+        image: "https://drive.google.com/uc?export=view&id=1H56UtNBx8tqvZ7SBxTFQt6X840bAOfXE"
     }
-    // new form 
-    const form = new FormData(product);
-    const data = {
-        sku: form.get(product.sku),
-        //quantity: Number(form.get('quantity')),
-    };
-
-    // create checkout
+ 
+    // Create Checkout 
     const response = await fetch('/.netlify/functions/create-checkout', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(newProduct),
     }).then((res) => res.json());
 
+    // Init Stripe with API Publishable key
     const stripe = Stripe(response.publishableKey);
-    const {
-        error
-    } = await stripe.redirectToCheckout({
-        sessionId: response.sessionId,
+
+    // Error Handling
+    const { error } = await stripe.redirectToCheckout({
+           sessionId: response.sessionId,
     });
 
     if (error) {
-        document
-            .querySelectorAll('button')
-            .forEach((button) => (button.disabled = false));
-        console.error(error);
-    }
-}
-
-
-customInputForm.addEventListener('submit', handleSubmit)
-//  => {
-
-    // disable all buttons 
-    // document.querySelectorAll('button').forEach((button) => (button.disabled = true));
-    
-    // // Prevent default form sub
-    // e.preventDefault();
-
-    // get the number inputs value and format the value to cents 
-    // const enteredAmount = document.querySelector('.custom-amount').value
-    // const formattedAmount = Math.round(enteredAmount * 100);
-
-    // const product = {
-    //     sku: "custom amount",
-    //     name: 'custom amount',
-    //     amount: formattedAmount,
-    //     currency: "USD"
-    // }
-    // log 
-    // console.log(product)
-// })
+           document
+               .querySelectorAll('button')
+               .forEach((button) => (button.disabled = false));
+           console.error(error);
+       }
+})
